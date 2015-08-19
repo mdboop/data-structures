287611594e45388dabb7181e612784cd6e50f633
mdboop <mdeanbrooks@gmail.com>
2015-08-19 15:56:31 -0700

    Add fully-working queue (functional sharing)

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -3,10 +3,30 @@ var Queue = function(){
   // but try not not reference your old code in writing the new style.
   var someInstance = {};
   _.extend(someInstance, queueMethods);
-
+  someInstance.highest = 0;
+  someInstance.storage = {};
   return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue : function(value) {
+    for(var key in this.storage) {
+      this.storage[Number(key) + 1] = this.storage[key];
+    }
+    this.highest++;
+    this.storage[0] = value;
+  },
+  dequeue : function() {
+    if (this.highest>0){
+      this.highest--;
+      var temp = this.storage[this.highest];
+      delete this.storage[this.highest];
+      return temp;
+    }
+  },
+  size : function() {
+    return this.highest;
+  } 
+};
 
 
