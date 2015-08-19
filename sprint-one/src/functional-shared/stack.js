569348a030b905d1b487311197e2a2aaef0f6a3e
mdboop <mdeanbrooks@gmail.com>
2015-08-19 15:37:17 -0700

    Add partially working stack (functional-shared)

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,36 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  _.extend(someInstance, stackMethods);
+  someInstance.storage = {};
+  someInstance.highest = 0;
+  return someInstance;
 };
 
-var stackMethods = {};
+
+
+var stackMethods = {
+  push : function(value){
+    //count up highest
+    this.highest++;
+    //add value as a property of storage at key highest
+    this.storage[this.highest] = value;
+  },
+
+  pop : function(){
+    if (this.highest>0){
+      this.highest--;
+      var temp = this.storage[this.highest];
+      delete this.storage[this.highest];
+      return temp;
+    }
+
+  },
+
+  size : function(){
+    return this.highest;
+  }
+};
 
 
