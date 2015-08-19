8805450a4d885b338551c5ab97a924c8a65443c7
mdboop <mdeanbrooks@gmail.com>
2015-08-19 16:15:27 -0700

    Add working stack (prototypal class)

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.storage = {};
+  someInstance.highest = 0;
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value){
+    this.storage[this.highest] = value;
+    this.highest++;
+  },
+  pop: function(){
+    if (this.highest>0){
+      this.highest--;
+      var temp = this.storage[this.highest];
+      delete this.storage[this.highest];
+      return temp;
+    }
+  },
+  size: function(){
+    return this.highest;
+  }
+};
 
 
