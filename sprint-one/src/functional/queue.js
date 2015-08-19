34c79835f8969754352c270241fec80185aeab61
mdboop <mdeanbrooks@gmail.com>
2015-08-19 15:19:11 -0700

    Add queue function

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,30 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var highest = 0;
+  var temp;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    highest++;
+    for(var key in storage) {
+      storage[Number(key) + 1] = storage[key];
+    }
+    storage[0] = value;
   };
 
   someInstance.dequeue = function(){
+    if(highest > 0) {
+      highest--;
+      temp = storage[highest];
+      delete storage[highest];
+      return temp;
+    }
   };
 
   someInstance.size = function(){
+    return highest;
   };
 
   return someInstance;
