4dbfc08a62da1acfadc8a4831b35d3e6b71f0056
mdboop <mdeanbrooks@gmail.com>
2015-08-19 15:07:41 -0700

    Add stack function

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,26 @@ var Stack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var highest = 0;
+  var temp;
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[highest] = value;
+    highest++;
   };
 
   someInstance.pop = function(){
+    if(highest > 0) {
+      highest--;
+      temp = storage[highest];
+      delete storage[highest];
+      return temp;
+    }
   };
 
   someInstance.size = function(){
+    return highest;
   };
 
   return someInstance;
