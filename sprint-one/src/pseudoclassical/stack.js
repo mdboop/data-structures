386309e389eab7e94b3d1c49de9063f44bf51150
mdboop <mdeanbrooks@gmail.com>
2015-08-19 16:36:24 -0700

    Add partially working stack (pseudo-classical)

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.highest = 0;
 };
+
+Stack.prototype.push = function(value){
+  this.storage[this.highest] = value;
+  this.highest++;
+};
+
+Stack.prototype.pop = function(){
+  if(this.highest > 0){
+    this.highest--;
+    var temp = this.storage[this.highest];
+    delete this.storage[this.highest];
+    return temp;
+  }
+};
+
+Stack.prototype.size = {
+  return this.highest;
+};
+
 
 
