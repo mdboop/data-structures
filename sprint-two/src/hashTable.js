a863570d3030f9848d102d9797f9bec4cc2b60d0
mdboop <mdeanbrooks@gmail.com>
2015-08-21 14:37:47 -0700

    Add partially working hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,30 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  //setting i and v in _storage
+  var tuple = [k,v];
+  this._storage.set(i,tuple);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var tuple = this._storage.get(i);
+  return tuple[1];
 };
 
 HashTable.prototype.remove = function(k){
-
+  var bucketIndex = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.each(function(bucket, currentIndex, storage){
+    if(currentIndex === bucketIndex) {
+      for(var j = 0; j < bucket.length; j++) {
+        if(bucket[j] === k) {
+          debugger;
+          //storage[bucketIndex].splice(j, 1);
+          storage[bucket][j] = null;
+        }
+      }
+    }
+  });
 };
 
 
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,22 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this.contains(item) ? this._storage = this._storage : this._storage.push(item);
 };
 
 setPrototype.contains = function(item){
+  return _.contains(this._storage, item);
 };
 
 setPrototype.remove = function(item){
+  var index = _.indexOf(this._storage, item);
+  this._storage.splice(index,1);
 };
 
 /*
