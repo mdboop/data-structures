c5eebaddace3e601d0a03dfc87af284dfa720aee
mdboop <mdeanbrooks@gmail.com>
2015-08-21 20:15:04 -0700

    Pass BinarySearchTree constructor methods tests

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,23 @@
 var BinarySearchTree = function(value){
+
 };
 
+BinarySearchTree.prototype.insert = function(value) {
 
+};
+
+BinarySearchTree.prototype.contains = function (value) {
+
+};
+
+BinarySearchTree.prototype.depthFirstLog = function() {
+
+};
+
+BinarySearchTree.prototype.Node = function(value) {
+  this.left = null;
+  this.right = null;
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
